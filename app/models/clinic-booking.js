1cf631964c255b70b5d7e3caf21894f5a8ae5207
Mal Ross <malcolm.ross4@nhs.net>
2026-02-25 15:56:01 +0000

    Add models for clinic bookings and appointments
    Each booking, which will have a unique booking reference number, has one or more appointments. This corresponds to one or more children being booked into a clinic by the same parent and in the same booking flow.
    
    Models are not yet hooked up to anything.

---
+++ app/models/clinic-booking.js
@@ -0,0 +1,114 @@
+import { fakerEN_GB as faker } from '@faker-js/faker'
+import { ClinicAppointment } from './clinic-appointment.js'
+
+/**
+ * @class ClinicBooking
+ * 
+ * @param {object} options - Options
+ * @param {object} [context] - Context
+ * 
+ * @property {object} [context] - Context
+ * @property {string} uuid - Clinic booking UUID
+ * @property {string} booking_reference - Booking reference number 
+ * 
+ * @property {string} [parentFullName] - Parent full name
+ * @property {string} [parentEmail] - Parent email
+ * @property {string} [parentPhone] - Parent phone number
+ * @property {boolean} [sms] - Get updates via SMS
+  * 
+ * @property {Array<ClinicAppointment>} [appointments] - Individual patients' appointments (one parent may book in multiple children under one booking)
+ */
+export class ClinicBooking {
+  constructor(options, context) {
+    this.context = context
+    this.uuid = options?.uuid || faker.string.uuid()
+    this.booking_reference = options?.booking_reference || `CLN-${faker.string.alphanumeric(8).toUpperCase()}`  // must be a better way to do this
+
+    this.parentFullName = options?.parentFullName
+    this.parentEmail = options?.parentEmail
+    this.parentPhone = options?.parentPhone
+    this.sms = options?.sms || false
+
+    this.appointments = options?.appointments || []
+  }
+
+  /**
+   * Get namespace
+   *
+   * @returns {string} Namespace
+   */
+  get ns() {
+    return 'clinicbooking'
+  }
+
+  /**
+   * Get URI
+   *
+   * @returns {string} URI
+   */
+  get uri() {
+    return `/clinic-bookings/${this.uuid}`
+  }
+
+  /**
+   * Find all
+   *
+   * @param {object} context - Context
+   * @returns {Array<ClinicBooking>|undefined} Clinic bookings
+   * @static
+   */
+  static findAll(context) {
+    return Object.values(context?.clinicBookings ?? {})
+      .map((booking) => new ClinicBooking(booking, context))
+  }
+
+  /**
+   * Find one
+   *
+   * @param {string} uuid - ClinicBooking UUID
+   * @param {object} context - Context
+   * @returns {ClinicBooking|undefined} Clinic booking
+   * @static
+   */
+  static findOne(uuid, context) {
+    if (context?.clinicBookings?.[uuid]) {
+      return new ClinicBooking(context.clinicBookings[uuid], context)
+    }
+  }
+
+  /**
+   * Update
+   *
+   * @param {string} uuid - ClinicBooking UUID
+   * @param {object} updates - Updates
+   * @param {object} context - Context
+   * @returns {ClinicBooking} Updated booking
+   * @static
+   */
+  static update(uuid, updates, context) {
+    const updatedBooking = Object.assign(this, updates)
+//    updatedBooking.updatedAt = today()
+
+    // Remove booking context
+    delete updatedBooking.context
+
+    // Delete original booking (with previous UUID)
+    delete context.clinicBookings[uuid]
+
+    // Update context
+    context.clinicBookings[updatedBooking.uuid] = updatedBooking
+
+    return updatedBooking
+  }
+
+  /**
+   * Delete
+   *
+   * @param {string} uuid - ClinicBooking UUID
+   * @param {object} context - Context
+   * @static
+   */
+  static delete(uuid, context) {
+    delete context.clinicBookings[uuid]
+  }
+}
