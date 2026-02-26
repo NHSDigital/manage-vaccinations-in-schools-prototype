9a264331c275bedc8a87f3ed5aa85f6aab8030ce
Mal Ross <malcolm.ross4@nhs.net>
2026-02-26 17:00:42 +0000

    Data generation functions for clinic bookings and appointments
    Squash this commit. As it stands, data file generation fails with a circular reference. This is because I made the mistake of composing the objects I was creating in a hierarchy rather than using IDs as foreign keys.
    
    I'll fix that in the next commit.

--- app/data.js
+++ app/data.js
@@ -1,5 +1,6 @@
 import vaccines from './datasets/vaccines.js'
 import batches from '../.data/batches.json' with { type: 'json' }
+import clinicBookings from '../.data/clinic-bookings.json' with { type: 'json' }
 import clinics from '../.data/clinics.json' with { type: 'json' }
 import instructions from '../.data/instructions.json' with { type: 'json' }
 import moves from '../.data/moves.json' with { type: 'json' }
@@ -30,6 +31,7 @@ const team = teams['001']
  */
 const data = {
   batches,
+  clinicBookings,
   clinics,
   counts: {},
   defaultBatches: {},
