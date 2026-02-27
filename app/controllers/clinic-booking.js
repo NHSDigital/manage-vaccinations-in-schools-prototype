c74491626b8216cfebb91a8436fcfd9ec30c378b
Mal Ross <malcolm.ross4@nhs.net>
2026-02-27 15:03:57 +0000

    Add initial routes and controller for clinic bookings.
    This is jsut based on the Move class/controller, so nothing major yet. Plus, I've commented out the Update method for now.
    
    Not sure yet whether I need to do the clinic appointments too. Suspect I will, of course, but need to get more comfortable with relative routes first.

---
+++ app/controllers/clinic-booking.js
@@ -0,0 +1,51 @@
+import _ from 'lodash'
+
+import { ClinicBooking } from '../models.js'
+import { getResults, getPagination } from '../utils/pagination.js'
+
+export const clinicBookingController = {
+  read(request, response, next, clinic_booking_uuid) {
+    response.locals.clinicBooking = ClinicBooking.findOne(clinic_booking_uuid, request.session.data)
+
+    next()
+  },
+
+  readAll(request, response, next) {
+    let clinicBookings = ClinicBooking.findAll(request.session.data)
+
+    // // Sort - not available yet
+    // clinicBookings = _.sortBy(clinicBookings, 'createdAt')
+
+    response.locals.clinicBookings = clinicBookings
+    response.locals.results = getResults(clinicBookings, request.query)
+    response.locals.pages = getPagination(clinicBookings, request.query)
+
+    next()
+  },
+
+  show(request, response) {
+    response.render('clinic-booking/show')
+  },
+
+  list(request, response) {
+    response.render('clinic-booking/list')
+  },
+
+  update(request, response) {
+    // const { decision } = request.body
+    const { data } = request.session
+    const { __, clinicBooking } = response.locals
+
+    // // Clean up session data
+    // delete data.decision
+
+    // // Ignore or switch schools
+    // decision === 'ignore'
+    //   ? clinicBooking.ignore(clinicBooking.uuid, data)
+    //   : clinicBooking.switch(clinicBooking.uuid, data)
+
+    // request.flash('success', __(`clinicBooking.${decision}.success`, { clinicBooking }))
+
+    response.redirect('/clinicBookings')
+  }
+}
