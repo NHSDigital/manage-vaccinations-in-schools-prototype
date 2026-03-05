494ebfd07e5d364b41b5283376e6b76ab512a501
Mal Ross <malcolm.ross4@nhs.net>
2026-03-05 14:31:16 +0000

    Move parent details for clinic bookings into clinic appointments
    And make them into full Parent objects, so that we get all of the other properties that I'd missed and can lean on things like summaryRows in the views.

--- app/generators/clinic-appointment.js
+++ app/generators/clinic-appointment.js
@@ -1,20 +1,23 @@
 import { fakerEN_GB as faker } from '@faker-js/faker'
 
-import { ClinicAppointment } from '../models.js'
-import { SessionType } from '../enums.js'
+import { ClinicAppointment, Parent } from '../models.js'
+import { ParentalRelationship, SessionType } from '../enums.js'
 import { addMinutes } from 'date-fns'
 import { getAge } from '../utils/date.js'
 import _ from 'lodash'
+import { generateParent } from './parent.js'
 
 const clinicSlotLength = Number(process.env.CLINIC_SLOT_LENGTH) || 10
 
 /**
  * Generate fake clinic appointment
- *
+ * 
+ * @param {string} booking_uuid The UUID of the booking this appointment belongs to
+ * @param {Parent|null} parentFromFirstAppointment The parent from the first appointment created in the same booking, if this appointment is not the first
  * @param {object} context The other data already defined (sessions, children, etc.)
  * @returns {ClinicAppointment} A new, fake clinic appointment
  */
-export function generateClinicAppointment(context) {
+export function generateClinicAppointment(booking_uuid, parentFromFirstAppointment, context) {
   const uuid = faker.string.uuid()
 
   // Choose a clinic session to book this appointment into
@@ -29,9 +32,9 @@ export function generateClinicAppointment(context) {
   const minAge = Math.min(allAgeLimits) || 4
   const maxAge = Math.max(allAgeLimits) || 15
 
-  // Child details, taken from children of an appropriate age
+  // Find/create a child of an appropriate age for the chosen clinic and its programmme
   let matchedPatient
-  if (faker.datatype.boolean(0.8)) {
+  if (faker.datatype.boolean(0.9)) {
     const eligiblePatients = Object.values(context.patients)
                                    .filter(p => {
                                      const age = getAge(p.dob)
@@ -40,9 +43,39 @@ export function generateClinicAppointment(context) {
     matchedPatient = faker.helpers.arrayElement(eligiblePatients)
   }
   const patient_uuid = matchedPatient?.uuid
+  // Unmatched child details, if required
   const unmatchedFirstName = matchedPatient ? undefined : faker.person.firstName()
   const unmatchedLastName = matchedPatient ? undefined : faker.person.lastName()
   const unmatchedDob = matchedPatient ? undefined : faker.date.birthdate({ min: minAge, max: maxAge, mode: 'age' })
+
+  // Parent details and relationship
+  let parent
+  if (!parentFromFirstAppointment) {
+    // This is the first appointment for the booking, so we're free to CREATE the parent details
+    if (matchedPatient) {
+      parent = (matchedPatient.parent1 || matchedPatient.parent2)
+        ?? generateParent(matchedPatient.lastName, faker.datatype.boolean(0.5))
+    } else {
+      parent = generateParent(unmatchedLastName, faker.datatype.boolean(0.5))
+    }
+  } else {
+    // Copy parent details from the first appointment and possibly adapt the relationship
+    parent = new Parent(parentFromFirstAppointment)
+    parent.uuid = faker.string.uuid()
+
+    // Set up the relationship to the child for this appointment
+    if ([ParentalRelationship.Mum, ParentalRelationship.Dad].includes(parentFromFirstAppointment.relationship)) {
+      // Mum or Dad initially, and most likely to stay that way
+      if (faker.datatype.boolean(0.1)) {
+        parent.relationship = faker.helpers.arrayElement([ParentalRelationship.Fosterer, ParentalRelationship.Guardian, ParentalRelationship.Other])
+        parent.relationshipOther = parent.relationship === ParentalRelationship.Other ? "Grandparent" : undefined
+      }
+    } else {
+      // Fosterer, Guardian or Other
+      parent.relationship = parentFromFirstAppointment.relationship
+      parent.relationshipOther = parentFromFirstAppointment.relationshipOther
+    }
+  }
 
   // Slot details (NB: session date is expected to specify midday)
   const startAt = addMinutes(clinicSession.date, faker.number.int({ min: 0, max: 60, multipleOf: clinicSlotLength }))
@@ -57,10 +90,12 @@ export function generateClinicAppointment(context) {
 
   return new ClinicAppointment({
     uuid,
+    booking_uuid,
     patient_uuid,
     unmatchedFirstName,
     unmatchedLastName,
     unmatchedDob,
+    parent,
     session_id,
     startAt,
     endAt,
