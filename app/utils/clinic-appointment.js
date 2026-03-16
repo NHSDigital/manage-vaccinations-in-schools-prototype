b8a6374a05b50ed5ea8f8a0956bb1b7aabeb3dce
Mal Ross <malcolm.ross4@nhs.net>
2026-03-16 10:14:44 +0000

    Add more views to the parents' clinic booking journey
    Note: this is not a working journey yet, but I'm committing before I end up adding an overwhelming amount in one go. (It's already too much, to be honest.)
    
    Changes in this commit:
     * Addition of clinic-finding and slot-booking views, plus parent detail views and health questions. Health questions not working yet due to selection of vaccines not binding properly (need to investigate correct use of decorate for checkboxes())
     * Introduction of a transaction object in the wizard context, for tracking transient journey data that doesn't need to be stored in the model e.g. number of children, location entered for search, etc. This will still need to be keyed to the booking uuid to avoid multi-user conflicts, however.
      * Various page text fixes, including access to the current child's first name for inclusion in question titles
     * Tenative change to how parent details are stored, as we really need to store them on the parent object in all appointments, not just one. So there's a parentTel getter/setter pair in ClinicBooking now that I might need to replicate for all relevant properties of the parent; or perhaps just offer a Parent object getter/setter pair and push changes to the ClinicAppointment objects (though what we should get back for parental responsibility is another matter, as that varies by appointment)

---
+++ app/utils/clinic-appointment.js
@@ -0,0 +1,52 @@
+import { camelToKebabCase } from './string.js'
+
+const getHealthQuestionPath = (key, pathPrefix) => {
+  return `${pathPrefix}health-question-${camelToKebabCase(key)}`
+}
+
+/**
+ * Get health question paths for given vaccines
+ *
+ * @param {string} pathPrefix - Path prefix
+ * @param {import('../models.js').ClinicAppointment} appointment - clinic appointment
+ * @returns {object} Health question paths
+ */
+export const getHealthQuestionPaths = (pathPrefix, appointment) => {
+  // Don't worry about it till we've actually made our first appointment
+  if (!appointment) {
+    return {}
+  }
+
+  const paths = {}
+  const healthQuestions = Object.entries(appointment.healthQuestionsForSelectedProgrammes)
+
+  healthQuestions.forEach(([key, question], index) => {
+    const questionPath = getHealthQuestionPath(key, pathPrefix)
+
+    if (question.conditional) {
+      const nextQuestion = healthQuestions[index + 1]
+      if (nextQuestion) {
+        const forkPath = getHealthQuestionPath(nextQuestion[0], pathPrefix)
+
+        paths[questionPath] = {
+          [forkPath]: {
+            data: `appointment.healthAnswers.${key}.answer`,
+            value: 'No'
+          }
+        }
+      } else {
+        paths[questionPath] = {}
+      }
+
+      // Add paths for conditional sub-questions
+      for (const subKey of Object.keys(question.conditional)) {
+        const subQuestionPath = getHealthQuestionPath(subKey, pathPrefix)
+        paths[subQuestionPath] = {}
+      }
+    } else {
+      paths[questionPath] = {}
+    }
+  })
+
+  return paths
+}
