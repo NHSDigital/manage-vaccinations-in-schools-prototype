16cb5cf5a93852c0c1ff1e5e573c6b7adafd5493
Mal Ross <malcolm.ross4@nhs.net>
2026-03-17 15:13:06 +0000

    Add homepage links for booking into clinic journeys
    One link per primary programme (aka session preset).

---
+++ app/utils/clinic-booking.js
@@ -0,0 +1,13 @@
+import { SessionPresets } from '../enums.js'
+
+/**
+ * Generate a URL for booking into a clinic whose primary programme is given by the session preset
+ *
+ * @param {string} sessionPresetName - the primary programme for the clinic
+ */
+export const getClinicBookingUrl = (sessionPresetName) => {
+  const sessionPreset = SessionPresets.find(
+    (preset) => preset.name === sessionPresetName
+  )
+  return `/book-into-a-clinic/${sessionPreset.slug}`
+}
