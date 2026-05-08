6c9f83e03adacdb5d62d09623cc2c556bd70abe8
Colin Rotherham <work@colinr.com>
2026-05-08 11:15:11 +0100

    Update TypeScript config

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -1,4 +1,4 @@
-import { initAll } from 'nhsuk-frontend'
+import { initAll } from '/nhsuk-frontend/nhsuk-frontend.min.js'
 
 // Initiate NHS.UK frontend components on page load
 document.addEventListener('DOMContentLoaded', () => {
