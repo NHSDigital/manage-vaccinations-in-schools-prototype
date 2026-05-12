d0fe10ff2d161ef43976a5b0f8da0d94148bf939
Colin Rotherham <work@colinr.com>
2026-05-12 08:37:43 +0100

    Extend more Express types

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -1,10 +1,45 @@
-import { Express } from 'express-serve-static-core'
 import 'connect-flash'
 import 'express-session'
+import {
+  ClinicBooking,
+  Consent,
+  Reply,
+  Session,
+  Team,
+  User
+} from '../../app/models.js'
+
+declare module 'express-serve-static-core' {
+  interface Locals extends ApplicationLocals {
+    data: Record<string, any> & ApplicationData
+  }
+
+  interface Request {
+    flash(message?: string): { [key: string]: string[] }
+    flash(event: string, message: string | string[]): any
+  }
+}
 
 declare module 'express-session' {
   interface SessionData {
-    data: Record<string, any>
+    data: Record<string, any> & ApplicationData
     referrer: string
   }
+}
+
+interface ApplicationData {
+  booking?: ClinicBooking
+  consent?: Consent
+  organisationName?: string
+  reply?: Reply
+  teams?: Team[]
+  team?: Team
+  token?: User
+  users?: User[]
+}
+
+interface ApplicationLocals extends ApplicationData {
+  bookingPhoneNumber?: string
+  currentPage?: string
+  session?: Session
 }
