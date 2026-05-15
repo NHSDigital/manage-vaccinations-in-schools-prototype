496696d5c469b1b11146b4722c36a40d505828d4
Colin Rotherham <work@colinr.com>
2026-05-15 16:38:56 +0100

    Add missing Express session data types

--- typings/express/index.d.ts
+++ typings/express/index.d.ts
@@ -3,6 +3,7 @@ import 'express-session'
 import {
   ClinicBooking,
   Consent,
+  PatientSession,
   Reply,
   Session,
   Team,
@@ -24,13 +25,27 @@ declare module 'express-session' {
 
 interface ApplicationData {
   booking?: ClinicBooking
+  cancellation?: Record<string, any>
+  clinicAdvert?: Record<string, any>
+  clinicInvite?: Record<string, any>
+  clinicPatient_ids?: string | string[]
   consent?: Consent
+  counts?: {
+    appointments: number
+    consents: number
+    review: number
+  }
   organisationName?: string
+  patientSession?: PatientSession
+  patientSession_uuid?: string
+  preScreen?: Record<string, any>
   reply?: Reply
-  teams?: Team[]
   team?: Team
+  teams?: Team[]
   token?: User
+  transaction?: Record<string, any>
   users?: User[]
+  wizard?: Record<string, any>
 }
 
 interface ApplicationLocals extends ApplicationData {
