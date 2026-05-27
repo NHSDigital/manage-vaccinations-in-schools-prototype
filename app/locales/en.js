904e520e4344bce09c30deadc758db96ca369e73
Mal Ross <malcolm.ross4@nhs.net>
2026-05-27 15:16:19 +0100

    Content updates for clinic booking process (#310)
    * Changes to content for various clinic-related pages
    
    Including:
     - less vague filter title
     - various parent booking pages
    
    * Replace guide dog with assistance animal
    
    ...and reinstate in the parents' clinic booking journey.
    
    * Clarification of commit button for creating sessions
    
    We'd seen in UR that people occasionally thought the button would trigger the journey for another new session (assuming that the session they were checking had already been created).
    
    * Content tidy-up for clinic booking
    
    Including:
     - confirmation page
     - start page
     - contact details page
     - impairments page

--- app/locales/en.js
+++ app/locales/en.js
@@ -318,15 +318,12 @@ export const en = {
       intro:
         'If your child has not been vaccinated at school, or is not up to date with their vaccinations for any other reason, you can book into a clinic.',
       programmes:
-        'Clinics have recently been set up to offer {{programmeNames}} vaccinations, but your child may be able to catch up on any outstanding vaccinations during their appointment.',
-      confirm: {
-        title: 'Book an appointment',
-        buttonText: 'Start now'
-      },
+        'Clinics have recently been set up to offer {{programmeNames}} vaccinations. However, at their appointment your child can also have vaccinations that they previously missed.',
+      confirm: 'Start now',
       otherMethods: {
         title: 'Other ways to book an appointment',
         description:
-          'The quickest way to book an appointment is online, using this service. This will take less than 5 minutes per child.\n\nIf you cannot use the service, you can book an appointment by phoning %s.'
+          'The quickest way to book an appointment is online, using this form. This will take less than 5 minutes.\n\nIf you cannot use this form, you can book an appointment by phoning %s.'
       }
     },
     childCount: {
@@ -543,7 +540,7 @@ export const en = {
         hint: 'Someone from the vaccinations team might call you if they have questions'
       },
       sms: {
-        label: 'Tick this box if you’d like to get updates by text message'
+        label: 'Confirm if you want appointment updates by text message'
       },
       contactPreference: {
         title: 'If we need to contact you',
@@ -584,7 +581,13 @@ export const en = {
     },
     confirmation: {
       title: 'Booking complete',
-      subtitle: '<p>Your reference number:<br><strong>%s</strong></p>'
+      subtitle: '<p>Your reference number:<br><strong>%s</strong></p>',
+      makeANote:
+        'Make a note of your booking reference number. You will need it if you change or cancel your appointment.',
+      whatNext: {
+        title: 'What happens next',
+        description: 'You’ll get a booking confirmation email or text message.'
+      }
     },
     show: {
       title: 'Manage your booking',
@@ -749,8 +752,8 @@ export const en = {
         label: 'Reasonable adjustments',
         title:
           'Will your child need any of the following adjustments during their vaccination?',
-        guideDog: {
-          label: 'A guide dog'
+        assistanceAnimal: {
+          label: 'Their assistance animal, for example a guide dog'
         },
         distraction: {
           label: 'A distraction while having the vaccination'
@@ -2391,7 +2394,7 @@ export const en = {
   search: {
     label: 'Search',
     hint: {
-      patient: 'Search by name, NHS number, postcode, and more',
+      patient: 'Search by name, NHS number or postcode',
       session: 'Search by location name or postcode'
     },
     advanced: 'Advanced filters',
@@ -2420,7 +2423,7 @@ export const en = {
     new: {
       label: 'Add a new session',
       'check-answers': {
-        confirm: 'Add session',
+        confirm: 'Add this session',
         title: 'Check and confirm'
       },
       success: '{{session.name}} created'
