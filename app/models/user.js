d19bd3b1cef6f52c3b8711d1a977f6a8c48cb27c
Mal Ross <malcolm.ross4@nhs.net>
2026-07-02 17:16:44 +0100

    Show the Review tab again for all roles but data consumer

--- app/models/user.js
+++ app/models/user.js
@@ -105,7 +105,7 @@ export class User {
           'patients',
           'schools',
           'sessions',
-          'review',
+          'reviews',
           'reports',
           'uploads',
           'downloads',
