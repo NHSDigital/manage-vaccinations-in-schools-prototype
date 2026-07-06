ab01eb344f26ce4c758df3f0deccdf3d0ce4bf3d
Mal Ross <malcolm.ross4@nhs.net>
2026-07-06 11:51:14 +0100

    Track the current user's team on response.locals
    This will allow us to easily present team contact details in parent-facing journeys.

--- app/middleware/authentication.js
+++ app/middleware/authentication.js
@@ -1,12 +1,16 @@
-import { User } from '../models.js'
+import { Team, User } from '../models.js'
 
 export const authentication = (request, response, next) => {
   const { data } = request.session
 
   // Get user from logged in user, or default to first user in session data
-  response.locals.account = data.token
+  const account = data.token
     ? new User(data.token, data)
     : User.findAll(data)[0]
+  response.locals.account = account
+
+  // Give pages access to the team (which isn't otherwise accessible via the account)
+  response.locals.team = Team.findOne(account.team_id, data)
 
   next()
 }
