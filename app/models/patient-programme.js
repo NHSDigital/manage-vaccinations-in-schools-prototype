a08458b9f5b10dea3584df1ef4695f35ec36608c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-07-07 16:44:43 +0100

    Use consistent date formatting for status notes

--- app/models/patient-programme.js
+++ app/models/patient-programme.js
@@ -543,14 +543,14 @@ export class PatientProgramme extends BaseModel {
           ? 'Not eligible for school age immunisation'
           : `Eligible from 1 September ${this.year}`
       case PatientStatus.Vaccinated:
-        return `Vaccinated on ${this.lastVaccinationGiven.formatted.administeredAt_dateShort}`
+        return `Vaccinated on ${this.lastVaccinationGiven.formatted.administeredAt_date}`
       case PatientStatus.Triage:
         return this.lastPatientSession.patientTriage
       case PatientStatus.Due:
         return this.lastPatientSession.vaccineCriteria
       case PatientStatus.Deferred:
         return this.lastVaccinationOutcome
-          ? `${this.lastPatientSession.patientDeferred} on ${this.lastVaccinationOutcome.formatted.administeredAt_dateShort}`
+          ? `${this.lastPatientSession.patientDeferred} on ${this.lastVaccinationOutcome.formatted.administeredAt_date}`
           : this.lastPatientSession.patientDeferred
       case PatientStatus.Refused:
         return this.lastPatientSession.patientRefused
