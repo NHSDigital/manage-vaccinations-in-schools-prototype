0d28c0ae4f661e5f722b09904964fa2f4de9690f
Martin Wright <martinjwright@gmail.com>
2026-07-09 13:49:47 +0100

    Add overview pages for each scenario

--- app/routes/parent-test.js
+++ app/routes/parent-test.js
@@ -19,35 +19,99 @@ router.get('/', (request, response) => {
 })
 
 router.get('/rosa', (request, response) => {
-  response.render('parent-test/rosa')
+  response.render('parent-test/rosa/index')
+})
+
+router.get('/rosa/mmr', (request, response) => {
+  response.render('parent-test/rosa/mmr/index')
+})
+
+router.get('/rosa/mmr/dose-1', (request, response) => {
+  response.render('parent-test/rosa/mmr/dose-1')
+})
+
+router.get('/rosa/mmr/not-counted', (request, response) => {
+  response.render('parent-test/rosa/mmr/not-counted')
 })
 
 router.get('/maya', (request, response) => {
-  response.render('parent-test/maya')
+  response.render('parent-test/maya/index')
+})
+
+router.get('/maya/mmr', (request, response) => {
+  response.render('parent-test/maya/mmr/index')
+})
+
+router.get('/maya/mmr/dose-1', (request, response) => {
+  response.render('parent-test/maya/mmr/dose-1')
+})
+
+router.get('/maya/mmr/dose-2', (request, response) => {
+  response.render('parent-test/maya/mmr/dose-2')
 })
 
 router.get('/theo', (request, response) => {
-  response.render('parent-test/theo')
+  response.render('parent-test/theo/index')
+})
+
+router.get('/theo/mmr', (request, response) => {
+  response.render('parent-test/theo/mmr/index')
+})
+
+router.get('/theo/mmr/dose-1', (request, response) => {
+  response.render('parent-test/theo/mmr/dose-1')
 })
 
 router.get('/priya', (request, response) => {
-  response.render('parent-test/priya')
+  response.render('parent-test/priya/index')
+})
+
+router.get('/priya/mmr', (request, response) => {
+  response.render('parent-test/priya/mmr/index')
 })
 
 router.get('/idris', (request, response) => {
-  response.render('parent-test/idris')
+  response.render('parent-test/idris/index')
+})
+
+router.get('/idris/mmr', (request, response) => {
+  response.render('parent-test/idris/mmr/index')
+})
+
+router.get('/idris/mmr/dose-1', (request, response) => {
+  response.render('parent-test/idris/mmr/dose-1')
 })
 
 router.get('/esme', (request, response) => {
-  response.render('parent-test/esme')
+  response.render('parent-test/esme/index')
+})
+
+router.get('/esme/mmr', (request, response) => {
+  response.render('parent-test/esme/mmr/index')
 })
 
 router.get('/caleb', (request, response) => {
-  response.render('parent-test/caleb')
+  response.render('parent-test/caleb/index')
+})
+
+router.get('/caleb/mmr', (request, response) => {
+  response.render('parent-test/caleb/mmr/index')
+})
+
+router.get('/caleb/mmr/dose-1', (request, response) => {
+  response.render('parent-test/caleb/mmr/dose-1')
 })
 
 router.get('/lena', (request, response) => {
-  response.render('parent-test/lena')
+  response.render('parent-test/lena/index')
+})
+
+router.get('/lena/mmr', (request, response) => {
+  response.render('parent-test/lena/mmr/index')
+})
+
+router.get('/lena/mmr/dose-1', (request, response) => {
+  response.render('parent-test/lena/mmr/dose-1')
 })
 
 router.get('/maya-overview', (request, response) => {
