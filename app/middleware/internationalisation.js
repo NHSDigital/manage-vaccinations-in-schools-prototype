3a76b8247c1240e67e8bc7962e090560e0cb0d2d
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-07-20 13:33:31 +0100

    Support localisation strings outside request/response cycle

--- app/middleware/internationalisation.js
+++ app/middleware/internationalisation.js
@@ -1,16 +1,6 @@
-import i18n from 'i18n'
-
-import { en } from '../locales/en.js'
+import i18n from '../utils/i18n.js'
 
 export const internationalisation = async (request, response, next) => {
-  i18n.configure({
-    cookie: 'locale',
-    defaultLocale: 'en',
-    objectNotation: true,
-    // @ts-ignore
-    staticCatalog: { en }
-  })
-
   i18n.init(request, response)
 
   next()
