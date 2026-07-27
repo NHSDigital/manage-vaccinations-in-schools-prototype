dfabc1e47dadc9291acddc9da220fcd59ed82b07
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-07-27 22:01:21 +0100

    Fix order of patient programme routes

--- app/routes/patient-programme.js
+++ app/routes/patient-programme.js
@@ -18,15 +18,15 @@ router.use((request, response, next) =>
 
 router.param('programme_id', patientProgramme.read)
 
-router.get('/:programme_id{/:view}', patientProgramme.show)
-
 router.post('/:programme_id/clinics', patientProgramme.addToSession)
-
-router.all('/:programme_id/new/:view', patientProgramme.readForm)
-router.get('/:programme_id/new/:view', patientProgramme.showForm('new'))
 
 router.get('/:programme_id/new/vaccination', patientProgramme.vaccinate('new'))
 router.get('/:programme_id/new/tetanus', patientProgramme.vaccinate('tetanus'))
 router.post('/:programme_id/new/triage', patientProgramme.triage)
+
+router.all('/:programme_id/new/:view', patientProgramme.readForm)
+router.get('/:programme_id/new/:view', patientProgramme.showForm('new'))
+
+router.get('/:programme_id{/:view}', patientProgramme.show)
 
 export const patientProgrammeRoutes = router
