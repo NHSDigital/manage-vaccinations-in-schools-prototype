0306b8501d5968d63ba7a7a086a556e06042b3a2
Mal Ross <malcolm.ross4@nhs.net>
2026-07-27 17:09:45 +0100

    Add new vs. edit parameters for most clinic booking route handlers

--- app/locales/en.js
+++ app/locales/en.js
@@ -920,7 +920,9 @@ export const en = {
         description: 'You’ll get a booking confirmation email or text message.'
       }
     },
-    success: 'Added {{fullName}} to the {{sessionName}}',
+    new: {
+      success: 'Added {{fullName}} to the {{sessionName}}'
+    },
     show: {
       title: 'Manage your booking',
       introduction:
@@ -941,6 +943,15 @@ export const en = {
         }
       },
       referenceNumber: 'Your booking reference number is: %s'
+    },
+    edit: {
+      title: 'Edit appointment',
+      summary: {
+        child: 'Child details',
+        appointment: 'Appointment details',
+        contact: 'Contact details'
+      },
+      success: 'Updated {{fullName}} in the {{sessionName}}'
     }
   },
   clinicVaccinationPeriod: {
