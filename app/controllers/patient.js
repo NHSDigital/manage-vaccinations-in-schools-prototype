2eb8eaa034efd7db3461d6ef447a4f17bf4fa13b
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-08-02 13:46:11 +0100

    Show full flu programme name on child record

--- app/controllers/patient.js
+++ app/controllers/patient.js
@@ -52,7 +52,7 @@ export const patientController = {
       ...Object.values(patient.activeProgrammes).map((patientProgramme) => {
         if (!account.isSchoolUser) {
           return {
-            text: patientProgramme.programme.name,
+            text: patientProgramme.name,
             href: patientProgramme.uri,
             current: currentPath === patientProgramme.uri
           }
