5ea035243c92ff396b71ef2b11d13243831a1a3a
Mal Ross <malcolm.ross4@nhs.net>
2026-08-04 09:43:54 +0100

    TODO: amend this commit

--- app/utils/clinic-appointment.js
+++ app/utils/clinic-appointment.js
@@ -101,6 +101,8 @@ export const getAllAppointmentPaths = (
     sessionData.appointment?.abandonmentReasons
   )
 
+  // TODO: check the journey type and leave out any pages that aren't part of editing accordingly
+
   // Note: the journey data will be unavailable on the confirmation page (which is parent-facing only)
   const journeyType =
     sessionData.journeyData[booking_uuid]?.journeyType ??
