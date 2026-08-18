8fe9299ffa846c1f17ac05685ee6f911a12fcfc7
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-08-18 23:23:52 +0100

    Use new heading option for notification banner

--- app/middleware/notification.js
+++ app/middleware/notification.js
@@ -5,18 +5,20 @@ export const notification = (request, response, next) => {
 
   response.locals.success = request.flash('success').map((text) => ({
     type: 'success',
-    html: filters(nunjucksEnv)
-      .nhsukMarkdown(text)
-      .replace('nhsuk-body', 'nhsuk-notification-banner__heading')
-      .replace('nhsuk-link', 'nhsuk-notification-banner__link')
+    heading: {
+      html: filters(nunjucksEnv)
+        .nhsukMarkdown(text, { inline: true })
+        .replace('nhsuk-link', 'nhsuk-notification-banner__link')
+    }
   }))[0]
 
   response.locals.message = request.flash('message').map((text) => ({
     title: 'Information',
-    html: filters(nunjucksEnv)
-      .nhsukMarkdown(text)
-      .replace('nhsuk-body', 'nhsuk-notification-banner__heading')
-      .replace('nhsuk-link', 'nhsuk-notification-banner__link')
+    heading: {
+      html: filters(nunjucksEnv)
+        .nhsukMarkdown(text, { inline: true })
+        .replace('nhsuk-link', 'nhsuk-notification-banner__link')
+    }
   }))[0]
 
   next()
