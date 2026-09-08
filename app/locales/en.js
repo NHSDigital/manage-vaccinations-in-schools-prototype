e61fce76e3945bc02bdf276f96c1d06bbee6dbf6
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-09-08 13:29:48 +0100

    Clarify that SAIS users just need to record a new consent to override the previous one from the same parent

--- app/locales/en.js
+++ app/locales/en.js
@@ -2486,6 +2486,8 @@ export const en = {
     },
     new: {
       title: 'Record a new consent response',
+      description:
+        'Recording a new consent from the same parent will override the previous one',
       'check-answers': {
         title: 'Check and confirm'
       },
